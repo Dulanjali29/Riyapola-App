c8d9e80b5c362406469d8959c2bb51b4ed20de62
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-03-20 13:46:45 +0530

    set stm token to local-storage

--- android/src/service/AxiosOrder.jsx
+++ android/src/service/AxiosOrder.jsx
@@ -1,5 +1,9 @@
 import axios from "axios";
 const token=localStorage.getItem('stmToken')
+const instance = axios.create({
+    baseURL:'http://localhost:8080/customer',
+    headers:{Authorization:`Bearer ${token}`}
 
+  });
 
   export default instance;
